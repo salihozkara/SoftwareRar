2d3045a479cc0b048755c968cf2fc72618e0941d
Salih <salihozkara00@gmail.com>
2022-01-26 22:36:45 +0300

    update

--- SoftwareRar/Program.cs
+++ SoftwareRar/Program.cs
@@ -3,91 +3,130 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json;
+using Microsoft.Win32;
 
 namespace SoftwareRar
 {
-    class Program
+    public class Program
     {
+
+
         static void Main(string[] args)
         {
-            if(args==null)
-                args = new[] { @"C:\Users\salih\Desktop\deeme\Yeni klasör" };
             NewMethod();
+            if (args == null || args.Length == 0) return;
+            if (args[0]=="-del")
+            {
+                Remove();
+                return;
+            }
             var mainPath = Path.GetFullPath(args[0]);
-            var zipFile = Directory.GetParent(mainPath)+"\\"+
-                          Path.GetFileName(mainPath)+@".zip"
+            var zipFile = Directory.GetParent(mainPath) + "\\" +
+                          Path.GetFileName(mainPath) + @".zip"
                           ;
-            Console.WriteLine(mainPath+Environment.NewLine+zipFile);
-            Console.WriteLine(zipFile);
-            var gitignores=Directory.GetFiles(mainPath, ".gitignore", SearchOption.AllDirectories);
-            Console.WriteLine(JsonSerializer.Serialize(gitignores));
-            var directories = Directory.GetDirectories(mainPath, "", SearchOption.AllDirectories);
-            var files = Directory.GetFiles(mainPath, "",SearchOption.AllDirectories);
-            //Console.WriteLine(JsonSerializer.Serialize(directories));
-            List<string> ignores = new();
-            
-            foreach (var gitignore in gitignores)
-            {
-                foreach (var line in File.ReadAllLines(gitignore))
-                {
-                    if (!line.Trim().StartsWith('#')&&!string.IsNullOrEmpty(line.Trim()))
-                    {
-                        ignores.Add(line.Trim());
-                    }
-                }
-            }
-            bas(ignores);
+            var gitIgnores = Directory.GetFiles(mainPath, ".gitignore", SearchOption.AllDirectories);
+            var files = Directory.GetFiles(mainPath, "", SearchOption.AllDirectories);
+            var ignores = (from gitignore in gitIgnores from line in File.ReadAllLines(gitignore) where !line.Trim().StartsWith('#') && !string.IsNullOrEmpty(line.Trim()) select line.Trim());
+
             using var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create);
-          
-            foreach (var file in files)
+
+            foreach (var file in files.Where(f => control(f, mainPath, ignores)))
             {
-                if (control(file,mainPath,ignores))
-                {
-                    archive.CreateEntryFromFile( file, file.Replace(mainPath+"\\",""));
-                }
-                
+                archive.CreateEntryFromFile(file, Path.GetFileName(mainPath)+"\\"+file.Replace(mainPath + "\\", ""));
             }
-            // Zip(zipFile, mainPath, "");
+            
         }
 
+        public static void Remove()
+        {
+            RemoveContext();
+            RemoveEnv();
+        }
+
+        #region MyRegion
+        const string name = "PATH";
+        const EnvironmentVariableTarget scope = EnvironmentVariableTarget.User; // or User
+        static string oldValue = Environment.GetEnvironmentVariable(name, scope);
+        static string path = AppDomain.CurrentDomain.BaseDirectory;
+
+        static string appName = AppDomain.CurrentDomain.FriendlyName + ".exe";
+        static string regName = AppDomain.CurrentDomain.FriendlyName;
+        static string menuName = "Folder\\shell\\" + regName;
+        static string command = $"Folder\\shell\\{regName}\\command";
+        const string contextName = "Zip ignore";
+        static string applicationName = (path + appName + " \"%1\"");
+
+
+        #endregion
         private static void NewMethod()
         {
-            const string name = "PATH";
-            const EnvironmentVariableTarget scope = EnvironmentVariableTarget.User; // or User
-            var oldValue = Environment.GetEnvironmentVariable(name, scope);
-            var path = Directory.GetCurrentDirectory();
-            if (oldValue != null && !oldValue.Contains(path))
+             //this is the pah to your program
+            var reg = Registry.ClassesRoot.OpenSubKey(command);
+            if (reg == null)
+                AddContext();
+            if (oldValue == null || oldValue.Contains(path)) return;
+            var newValue = oldValue + @";" + path + ";";
+            Environment.SetEnvironmentVariable(name, newValue, scope);
+
+            
+        }
+
+        static void RemoveEnv()
+        {
+            if (oldValue == null || oldValue.Contains(path)) return;
+            var newValue = oldValue.Replace(@";" + path + ";", "");
+            Environment.SetEnvironmentVariable(name, newValue, scope);
+        }
+        static void AddContext()
+        {
+            RegistryKey regMenu = null;
+            RegistryKey regCmd = null;
+            try
             {
-                var newValue = oldValue + @";" + path + ";";
-                Environment.SetEnvironmentVariable(name, newValue, scope);
+                regMenu = Registry.ClassesRoot.CreateSubKey(menuName);
+                regMenu?.SetValue("", contextName);
+                regCmd = Registry.ClassesRoot.CreateSubKey(command);
+                regCmd?.SetValue("", applicationName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                regMenu?.Close();
+                regCmd?.Close();
             }
         }
 
-        private static bool control(string file,string mainPath, IEnumerable<string> ignores)
+        static void RemoveContext()
         {
-            file = file.Replace(mainPath , "").Replace("/","\\");
-            return !ignores.Any(ignore =>file.StartsWith(ignore.Replace("/","\\")) );
-        }
-        private static void bas(object obj)
-        {
-            Console.WriteLine(JsonSerializer.Serialize(obj));
-        }
-        private static void Zip(string zipFile, string mainPath, string basePath)
-        {
-            using var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create);
-            foreach (var fPath in Directory.GetFiles(mainPath))
+            try
             {
-                try
+                var reg = Registry.ClassesRoot.OpenSubKey(command);
+                if (reg != null)
                 {
-                    archive.CreateEntryFromFile( fPath, basePath +Path.GetFileName(fPath));
-                    
+                    reg.Close();
+                    Registry.ClassesRoot.DeleteSubKey(command);
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
+                reg = Registry.ClassesRoot.OpenSubKey(menuName);
+                if (reg == null) return;
+                reg.Close();
+                Registry.ClassesRoot.DeleteSubKey(menuName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine
+                    (ex.Message);
             }
         }
+        private static bool control(string file, string mainPath, IEnumerable<string> ignores)
+        {
+            file = file.Replace(mainPath, "").Replace("/", "\\");
+            return !ignores.Any(ignore => file.StartsWith(ignore.Replace("/", "\\")));
+        }
+
     }
 }
